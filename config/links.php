e7d14b6015fdb55574bdfaee5809b85b6ead1c7a
DenisD3D <dd.denis2544@gmail.com>
2024-08-28 12:37:17 +0200

    Switch to astro

--- config/links.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'github' => 'https://github.com/Denis3d/',
-    'twitter' => '#',
-    'discord' => 'https://discord.gg/rzzd76c',
-    'mail' => 'mailto:contact@denis3d.ml',
-    'ads_url' => 'http://test-ads2.denisd3d.cf/serve/sitelinks/?id=497313&url='
-];
--- resources/views/contact_email_template.blade.php
+++
@@ -1,3 +0,0 @@
-<h2>New Message from {{ $data['name'] }} (Email : {{ $data['email'] }})</h2>
-<hr>
-<p>{{ $data['message'] }}</p>
